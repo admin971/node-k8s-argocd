61f9fc862d15a72a930de26039e3801775d7a162
root <root@ip-172-31-0-202.ap-southeast-2.compute.internal>
2025-04-22 12:56:40 +0000

    fds

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from Node.js on K8s via ArgoCD final cheack automatic));
+app.get('/', (req, res) => res.send('Hello from Node.js on K8s via ArgoCD final cheackwith karthick  automatic));
 app.listen(port, () => console.log(`App running on port ${port}`));
 
